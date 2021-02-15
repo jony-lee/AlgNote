32ce2830697241fa5fb243e76e45675fe048f921
lichong <lichong_i@didichuxing.com>
2021-02-15 17:38:33 +0800

    done 90

--- easy485/main.go
+++ easy485/main.go
@@ -1,4 +1,5 @@
-package main
+package leetcode
+
 // @File    :   main.go
 // @Time    :   2021/02/15 15:36:03
 // @Author  :   jony
@@ -7,18 +8,18 @@ package main
 //https://leetcode-cn.com/problems/max-consecutive-ones/submissions/
 
 func findMaxConsecutiveOnes(nums []int) int {
-    var max = 0
-    var length = 0
-    for i:= 0;i< len(nums);i++{
-        if nums[i]==1{
-            length++
-        }
-        if max < length {
-            max = length
-        }
-        if nums[i]!= 1{
-            length = 0
-        }
-    }
-    return max
+	var max = 0
+	var length = 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 1 {
+			length++
+		}
+		if max < length {
+			max = length
+		}
+		if nums[i] != 1 {
+			length = 0
+		}
+	}
+	return max
 }
